cded181d3f90c21079ca295f49eaf41ac4853b2c
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:22:18 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,4 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "metadata" {
+  value       = local.enabled ? helm_release.this[0].metadata : null
+  description = "Block status of the deployed release"
 }
